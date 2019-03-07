0a21fb89f01448619f5e083a0f61a9d40cbd52d0
jinyulinlnag <jinyulinlang@126.com>
2019-03-07 23:00:57 +0800

    dfsasdA

--- springcloud-provider-dept-8001/src/test/java/com/gao/dept/App8001Test.java
+++ springcloud-provider-dept-8001/src/test/java/com/gao/dept/App8001Test.java
@@ -4,5 +4,8 @@ public class App8001Test {
     public static void main(String[] args) {
         System.out.println ("dagdaga");
         System.out.println ("dagdaga");
+        System.out.println ("dagdaga");
+        System.out.println ("dagdaga");
+        System.out.println ("dagdaga");
     }
 }
