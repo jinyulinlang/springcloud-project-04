85626e84f9643c1d1d994978d66d19921312686b
jinyulinlnag <jinyulinlang@126.com>
2019-03-07 23:00:44 +0800

    dfsasdA

--- springcloud-provider-dept-8001/src/test/java/com/gao/dept/App8001Test.java
+++ springcloud-provider-dept-8001/src/test/java/com/gao/dept/App8001Test.java
@@ -3,5 +3,6 @@ package com.gao.dept;
 public class App8001Test {
     public static void main(String[] args) {
         System.out.println ("dagdaga");
+        System.out.println ("dagdaga");
     }
 }
